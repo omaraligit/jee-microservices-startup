d8b3e1c4e5687846748157e0696ae1a7baf6965f
Omar Ali <omarali0601520286@gmail.com>
2020-08-08 21:33:21 +0100

    intruducing spring-cloud-starter=kubernetes

--- apiGateway/src/main/java/dev/cdojo/apiGateway/ApiGatewayApplication.java
+++ apiGateway/src/main/java/dev/cdojo/apiGateway/ApiGatewayApplication.java
@@ -12,11 +12,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 @EnableHystrixDashboard
 @EnableEurekaServer
 @EnableAdminServer
+@EnableDiscoveryClient
+
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
